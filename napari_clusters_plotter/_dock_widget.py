6c429f75e9292e149197ce41f15d848e338b9fd2
Laura Žigutytė <zigutyte@gmail.com>
2021-11-15 11:38:38 +0100

    Update _dock_widget.py

---
+++ napari_clusters_plotter/_dock_widget.py
@@ -0,0 +1,10 @@
+from napari_plugin_engine import napari_hook_implementation
+from ._measure import MeasureWidget
+from ._plotter import PlotterWidget
+from ._umap import UMAPWidget
+from ._kmeans_clustering import ClusteringWidget
+
+
+@napari_hook_implementation
+def napari_experimental_provide_dock_widget():
+    return [MeasureWidget, PlotterWidget, UMAPWidget, ClusteringWidget]
