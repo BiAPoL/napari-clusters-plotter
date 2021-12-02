fda5a6c9db56fba7ecf7fd956c7dbdc68004c18e
Laura Žigutytė <zigutyte@gmail.com>
2021-12-02 21:59:37 +0100

    Reformat the file, add restore_default function, make some variables names nicer

--- napari_clusters_plotter/_utilities.py
+++ napari_clusters_plotter/_utilities.py
@@ -13,23 +13,27 @@ def show_table(viewer, labels_layer):
 
 
 # function for generating image labelled by clusters given the label image and the cluster prediction list
-def generate_parametric_cluster_image(labelimage, predictionlist):
+def generate_parametric_cluster_image(label_image, prediction_list):
     print('Generation of parametric cluster image started')
 
-    # reforming the prediction list this is done to account for cluster labels that start at 0
-    # conveniently hdbscan labelling starts at -1 for noise, removing these from the labels
-    predictionlist_new = np.array(predictionlist) + 1
+    # reforming the prediction list; this is done to account for cluster labels that start at 0
+    prediction_list_new = np.array(prediction_list) + 1
 
     # this takes care of the background label that needs to be 0 as well as any other
     # labels that might have been accidentally deleted
-    for i in range(int(np.min(labelimage[np.nonzero(labelimage)]))):
-        predictionlist_new = np.insert(predictionlist_new, i, 0)
+    for i in range(int(np.min(label_image[np.nonzero(label_image)]))):
+        prediction_list_new = np.insert(prediction_list_new, i, 0)
 
     # pushing of variables into GPU
-    cle_list = cle.push(predictionlist_new)
-    cle_labels = cle.push(labelimage)
+    cle_list = cle.push(prediction_list_new)
+    cle_labels = cle.push(label_image)
 
     # generation of cluster label image
     parametric_image = cle.pull(cle.replace_intensities(cle_labels, cle_list))
     print('Generation of parametric cluster image finished')
     return np.array(parametric_image, dtype="int64")
+
+
+def restore_defaults(widget, defaults: dict):
+    for item, val in defaults.items():
+        getattr(widget, item).value = val
