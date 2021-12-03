b3a54f3bdcf28d656382a4140038774b3765230d
Laura Žigutytė <zigutyte@gmail.com>
2021-12-03 20:30:32 +0100

    Use magicgui for layers selection in Measuring widget, reformat file, change some variable names for better readability

--- napari_clusters_plotter/_measure.py
+++ napari_clusters_plotter/_measure.py
@@ -2,45 +2,41 @@ import pyclesperanto_prototype as cle
 import pandas as pd
 import numpy as np
 import warnings
-import napari
-from magicgui.widgets import FileEdit
+from napari.layers import Labels, Image
+from magicgui.widgets import FileEdit, create_widget
 from magicgui.types import FileDialogMode
 from qtpy.QtWidgets import QWidget, QPushButton, QLabel, QHBoxLayout, QVBoxLayout, QComboBox, QLineEdit
 from ._utilities import show_table, widgets_inactive
-from napari_tools_menu import  register_dock_widget
+from napari_tools_menu import register_dock_widget
+
 
 @register_dock_widget(menu="Measurement > Measure intensity, shape and neighbor counts (ncp)")
-
 class MeasureWidget(QWidget):
 
     def __init__(self, napari_viewer):
         super().__init__()
 
         self.viewer = napari_viewer
-
-        napari_viewer.layers.selection.events.changed.connect(self._on_selection)
-
-        self.current_annotation = None
-
-        # QVBoxLayout - lines up widgets vertically
         self.setLayout(QVBoxLayout())
-        label_container = QWidget()
-        label_container.setLayout(QVBoxLayout())
-        label_container.layout().addWidget(QLabel("<b>Measurement</b>"))
 
-        # selection of image layer
-        choose_img_container = QWidget()
-        choose_img_container.setLayout(QHBoxLayout())
-        self.image_list = QComboBox()
-        self.update_image_list()
-        choose_img_container.layout().addWidget(QLabel("Image layer"))
-        choose_img_container.layout().addWidget(self.image_list)
+        title_container = QWidget()
+        title_container.setLayout(QVBoxLayout())
+        title_container.layout().addWidget(QLabel("<b>Measurement</b>"))
 
-        # selection of labels layer
-        self.label_list = QComboBox()
-        self.update_label_list()
-        choose_img_container.layout().addWidget(QLabel("Labels layer"))
-        choose_img_container.layout().addWidget(self.label_list)
+        self.image_select = create_widget(annotation=Image, label="image_layer")
+        self.labels_select = create_widget(annotation=Labels, label="labels_layer")
+
+        # widget for the selection of image layer
+        image_layer_selection_container = QWidget()
+        image_layer_selection_container.setLayout(QHBoxLayout())
+        image_layer_selection_container.layout().addWidget(QLabel("Image layer"))
+        image_layer_selection_container.layout().addWidget(self.image_select.native)
+
+        # widget for the selection of labels layer
+        labels_layer_selection_container = QWidget()
+        labels_layer_selection_container.setLayout(QHBoxLayout())
+        labels_layer_selection_container.layout().addWidget(QLabel("Labels layer"))
+        labels_layer_selection_container.layout().addWidget(self.labels_select.native)
 
         # selection if region properties should be measured now or uploaded from file
         reg_props_container = QWidget()
@@ -52,14 +48,14 @@ class MeasureWidget(QWidget):
         reg_props_container.layout().addWidget(self.reg_props_choice_list)
 
         # region properties file upload
-        self.regpropsfile_widget = QWidget()
-        self.regpropsfile_widget.setLayout(QVBoxLayout())
+        self.reg_props_file_widget = QWidget()
+        self.reg_props_file_widget.setLayout(QVBoxLayout())
         filename_edit = FileEdit(
             mode=FileDialogMode.EXISTING_FILE,
             filter='*.csv',
             value="  ")
-        self.regpropsfile_widget.layout().addWidget(filename_edit.native)
-        self.regpropsfile_widget.setVisible(False)
+        self.reg_props_file_widget.layout().addWidget(filename_edit.native)
+        self.reg_props_file_widget.setVisible(False)
 
         # average distance of n closest points list
         self.closest_points_container = QWidget()
@@ -71,37 +67,38 @@ class MeasureWidget(QWidget):
         self.closest_points_container.setVisible(False)
 
         # Run button
-        run_widget = QWidget()
-        run_widget.setLayout(QHBoxLayout())
+        run_button_container = QWidget()
+        run_button_container.setLayout(QHBoxLayout())
         button = QPushButton("Run")
 
+        # adding all widgets to the layout
+        self.layout().addWidget(title_container)
+        self.layout().addWidget(image_layer_selection_container)
+        self.layout().addWidget(labels_layer_selection_container)
+        self.layout().addWidget(reg_props_container)
+        self.layout().addWidget(self.reg_props_file_widget)
+        self.layout().addWidget(self.closest_points_container)
+        self.layout().addWidget(run_button_container)
+        self.layout().setSpacing(0)
+
         def run_clicked():
-            if self.get_selected_label() is None:
+            if self.labels_select.value is None:
                 warnings.warn("No labels image was selected!")
                 return
 
-            if self.get_selected_image() is None:
+            if self.image_select.value is None:
                 warnings.warn("No image was selected!")
                 return
 
             self.run(
-                self.get_selected_image().data,
-                self.get_selected_label().data,
+                self.image_select.value,
+                self.labels_select.value,
                 str(filename_edit.value.absolute()).replace("\\", "/").replace("//", "/"),
                 self.closest_points_list.text(),
             )
 
         button.clicked.connect(run_clicked)
-        run_widget.layout().addWidget(button)
-
-        # adding all widgets to the layout
-        self.layout().addWidget(label_container)
-        self.layout().addWidget(choose_img_container)
-        self.layout().addWidget(reg_props_container)
-        self.layout().addWidget(self.regpropsfile_widget)
-        self.layout().addWidget(self.closest_points_container)
-        self.layout().addWidget(run_widget)
-        self.layout().setSpacing(0)
+        run_button_container.layout().addWidget(button)
 
         # go through all widgets and change spacing
         for i in range(self.layout().count()):
@@ -113,62 +110,17 @@ class MeasureWidget(QWidget):
         self.reg_props_choice_list.currentIndexChanged.connect(self.change_reg_props_file)
         self.reg_props_choice_list.currentIndexChanged.connect(self.change_closest_points_list)
 
-    def get_selected_image(self):
-        index = self.image_list.currentIndex()
-        if index >= 0:
-            return self._available_images[index]
-        return None
+    def showEvent(self, event) -> None:
+        super().showEvent(event)
+        self.reset_choices()
 
-    def get_selected_label(self):
-        index = self.label_list.currentIndex()
-        if index >= 0:
-            return self._available_labels[index]
-        return None
-
-    def update_label_list(self):
-        selected_layer = self.get_selected_label()
-        selected_index = -1
-
-        self._available_labels = []
-        self.label_list.clear()
-        i = 0
-        for layer in self.viewer.layers:
-            if isinstance(layer, napari.layers.Labels):
-                self._available_labels.append(layer)
-                if layer == selected_layer:
-                    selected_index = i
-                self.label_list.addItem(layer.name)
-                i = i + 1
-        self.label_list.setCurrentIndex(selected_index)
-
-    def update_image_list(self):
-        selected_layer = self.get_selected_image()
-        selected_index = -1
-
-        self._available_images = []
-        self.image_list.clear()
-        i = 0
-        for layer in self.viewer.layers:
-            if isinstance(layer, napari.layers.Image):
-                self._available_images.append(layer)
-                if layer == selected_layer:
-                    selected_index = i
-                self.image_list.addItem(layer.name)
-                i = i + 1
-        self.image_list.setCurrentIndex(selected_index)
-
-    def _on_selection(self, event=None):
-        num_labels_in_viewer = len([layer for layer in self.viewer.layers if isinstance(layer, napari.layers.Labels)])
-        if num_labels_in_viewer != self.label_list.size():
-            self.update_label_list()
-
-        num_images_in_viewer = len([layer for layer in self.viewer.layers if isinstance(layer, napari.layers.Image)])
-        if num_images_in_viewer != self.image_list.size():
-            self.update_image_list()
+    def reset_choices(self, event=None):
+        self.image_select.reset_choices(event)
+        self.labels_select.reset_choices(event)
 
     # toggle widgets visibility according to what is selected
     def change_reg_props_file(self):
-        widgets_inactive(self.regpropsfile_widget,
+        widgets_inactive(self.reg_props_file_widget,
                          active=self.reg_props_choice_list.currentText() == 'Upload file')
 
     def change_closest_points_list(self):
@@ -176,31 +128,28 @@ class MeasureWidget(QWidget):
                          active=self.reg_props_choice_list.currentText() == 'Measure now (with neighborhood data)')
 
     # this function runs after the run button is clicked
-    def run(self, image, labels, regpropsfile, n_closest_points_str):
+    def run(self, image_layer, labels_layer, reg_props_file, n_closest_points_str):
         print("Measurement running")
-
-        labels_layer = self.get_selected_label()
 
         # depending on settings,...
         region_props_source = self.reg_props_choice_list.currentText()
 
         if region_props_source == 'Upload file':
             # load region properties from csv file
-            reg_props = pd.read_csv(regpropsfile)
+            reg_props = pd.read_csv(reg_props_file)
             try:
-                edited_regprops = reg_props.drop(['Unnamed: 0'], axis = 1)
+                edited_reg_props = reg_props.drop(['Unnamed: 0'], axis=1)
             except KeyError:
-                edited_regprops = reg_props
-            
-            if 'labels' not in edited_regprops.keys().tolist():
-                label_column = pd.DataFrame({'label':np.array(range(1,(len(edited_regprops)+1)))})
-                reg_props_w_labels = pd.concat([label_column,edited_regprops], axis = 1)
-                
-            labels_layer.properties = reg_props_w_labels
+                edited_reg_props = reg_props
+
+            if 'labels' not in edited_reg_props.keys().tolist():
+                label_column = pd.DataFrame({'label': np.array(range(1, (len(edited_reg_props) + 1)))})
+                reg_props_w_labels = pd.concat([label_column, edited_reg_props], axis=1)
+                labels_layer.properties = reg_props_w_labels
 
         elif 'Measure now' in region_props_source:
             # or determine it now using clEsperanto
-            reg_props = cle.statistics_of_labelled_pixels(image, labels)
+            reg_props = cle.statistics_of_labelled_pixels(image_layer.data, labels_layer.data)
 
             # and select columns, depending on if intensities and/or shape were selected
             columns = ['label', 'centroid_x', 'centroid_y', 'centroid_z']
@@ -219,11 +168,10 @@ class MeasureWidget(QWidget):
                 labels_layer.properties = reg_props
 
             if 'neighborhood' in region_props_source:
-
                 n_closest_points_split = n_closest_points_str.split(",")
                 n_closest_points_list = map(int, n_closest_points_split)
                 print("regionprops_with_neighborhood_data function got " + str(columns))
-                reg_props = regionprops_with_neighborhood_data(columns, labels, n_closest_points_list, reg_props)
+                reg_props = regionprops_with_neighborhood_data(columns, labels_layer, n_closest_points_list, reg_props)
 
                 labels_layer.properties = reg_props
 
@@ -237,13 +185,12 @@ class MeasureWidget(QWidget):
 
 
 def regionprops_with_neighborhood_data(columns, label_image, n_closest_points_list, reg_props):
-
     # get lowest label index to adjust sizes of measurement arrays
     min_label = int(np.min(label_image[np.nonzero(label_image)]))
 
     columns = columns + ['min_intensity', 'max_intensity', 'sum_intensity',
-               'mean_intensity', 'standard_deviation_intensity', 'area', 'mean_distance_to_centroid',
-               'max_distance_to_centroid', 'mean_max_distance_to_centroid_ratio']
+                         'mean_intensity', 'standard_deviation_intensity', 'area', 'mean_distance_to_centroid',
+                         'max_distance_to_centroid', 'mean_max_distance_to_centroid_ratio']
 
     region_props = {column: value for column, value in reg_props.items() if column in columns}
 
@@ -265,14 +212,14 @@ def regionprops_with_neighborhood_data(columns, label_image, n_closest_points_li
     cle.set_column(touching_neighbor_count, 0, 0)
 
     # conversion and editing of the distance matrix, so that it does not break cle.average_distance
-    viewdist_mat = cle.pull(distance_matrix)
-    tempdist_mat = np.delete(viewdist_mat, range(min_label), axis=0)
-    edited_distmat = np.delete(tempdist_mat, range(min_label), axis=1)
+    view_dist_mat = cle.pull(distance_matrix)
+    temp_dist_mat = np.delete(view_dist_mat, range(min_label), axis=0)
+    edited_dist_mat = np.delete(temp_dist_mat, range(min_label), axis=1)
 
     # iterating over different neighbor numbers for average neighbor distance calculation
     for i in n_closest_points_list:
         distance_of_n_closest_points = \
-            cle.pull(cle.average_distance_of_n_closest_points(cle.push(edited_distmat), n=i))[0]
+            cle.pull(cle.average_distance_of_n_closest_points(cle.push(edited_dist_mat), n=i))[0]
 
         # addition to the regionprops dictionary
         region_props['avg distance of {val} closest points'.format(val=i)] = distance_of_n_closest_points
