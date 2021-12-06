fa524e3882b7c0a5c16b5ed0906f49776d2a3231
Laura Žigutytė <zigutyte@gmail.com>
2021-12-06 21:56:01 +0100

    Use magicgui for labels layer selection in dimensionality reduction widget, add restore defaults and update measurements buttons, add warnings

--- napari_clusters_plotter/_dimensionality_reduction.py
+++ napari_clusters_plotter/_dimensionality_reduction.py
@@ -1,14 +1,22 @@
+from functools import partial
+
 import pandas as pd
 import warnings
-import napari
+from napari.layers import Labels
+from magicgui.widgets import create_widget
 from qtpy.QtWidgets import QWidget, QPushButton, QLabel, QHBoxLayout, QVBoxLayout
-from qtpy.QtWidgets import QListWidget, QListWidgetItem, QAbstractItemView, QComboBox, QSpinBox
+from qtpy.QtWidgets import QListWidget, QListWidgetItem, QAbstractItemView, QComboBox
 from qtpy.QtCore import QRect
-from ._utilities import widgets_inactive
+from ._utilities import widgets_inactive, restore_defaults
 from napari_tools_menu import register_dock_widget
 
 # Remove when the problem is fixed from sklearn side
 warnings.filterwarnings(action='ignore', category=FutureWarning, module='sklearn')
+
+DEFAULTS = dict(
+    n_neighbors=15,
+    perplexity=30,
+)
 
 
 @register_dock_widget(menu="Measurement > Dimensionality reduction (ncp)")
@@ -19,31 +27,26 @@ class DimensionalityReductionWidget(QWidget):
 
         self.viewer = napari_viewer
 
-        napari_viewer.layers.selection.events.changed.connect(self._on_selection)
-
-        self.current_annotation = None
-
         # QVBoxLayout - lines up widgets vertically
         self.setLayout(QVBoxLayout())
         label_container = QWidget()
         label_container.setLayout(QVBoxLayout())
         label_container.layout().addWidget(QLabel("<b>Dimensionality reduction</b>"))
 
-        # selection of labels layer
-        choose_img_container = QWidget()
-        choose_img_container.setLayout(QHBoxLayout())
-        self.label_list = QComboBox()
-        self.update_label_list()
-        self.label_list.currentIndexChanged.connect(self.update_properties_list)
-        choose_img_container.layout().addWidget(QLabel("Labels layer"))
-        choose_img_container.layout().addWidget(self.label_list)
+        # widget for the selection of labels layer
+        labels_layer_selection_container = QWidget()
+        labels_layer_selection_container.setLayout(QHBoxLayout())
+        labels_layer_selection_container.layout().addWidget(QLabel("Labels layer"))
+        self.labels_select = create_widget(annotation=Labels, label="labels_layer")
+
+        labels_layer_selection_container.layout().addWidget(self.labels_select.native)
 
         # selection of dimension reduction algorithm
         algorithm_container = QWidget()
         algorithm_container.setLayout(QHBoxLayout())
         algorithm_container.layout().addWidget(QLabel("Dimensionality Reduction Algorithm"))
         self.algorithm_choice_list = QComboBox()
-        self.algorithm_choice_list.addItems(['   ', 'UMAP', 't-SNE'])
+        self.algorithm_choice_list.addItems(['', 'UMAP', 't-SNE'])
         algorithm_container.layout().addWidget(self.algorithm_choice_list)
 
         # selection of n_neighbors - The size of local neighborhood (in terms of number of neighboring sample points)
@@ -52,22 +55,24 @@ class DimensionalityReductionWidget(QWidget):
         self.n_neighbors_container = QWidget()
         self.n_neighbors_container.setLayout(QHBoxLayout())
         self.n_neighbors_container.layout().addWidget(QLabel("Number of neighbors"))
-        self.n_neighbors = QSpinBox()
-        self.n_neighbors.setMinimumWidth(40)
-        self.n_neighbors.setMinimum(2)
-        self.n_neighbors.setValue(15)
-        self.n_neighbors_container.layout().addWidget(self.n_neighbors)
+        self.n_neighbors = create_widget(widget_type="SpinBox",
+                                         name='n_neighbors',
+                                         value=DEFAULTS['n_neighbors'],
+                                         options=dict(min=2, step=1))
+
+        self.n_neighbors_container.layout().addWidget(self.n_neighbors.native)
         self.n_neighbors_container.setVisible(False)
 
         # selection of the level of perplexity. Higher values should be chosen when handling large datasets
         self.perplexity_container = QWidget()
         self.perplexity_container.setLayout(QHBoxLayout())
         self.perplexity_container.layout().addWidget(QLabel("Perplexity"))
-        self.perplexity = QSpinBox()
-        self.perplexity.setMinimumWidth(40)
-        self.perplexity.setMinimum(1)
-        self.perplexity.setValue(30)
-        self.perplexity_container.layout().addWidget(self.perplexity)
+        self.perplexity = create_widget(widget_type="SpinBox",
+                                        name='perplexity',
+                                        value=DEFAULTS['perplexity'],
+                                        options=dict(min=1, step=1))
+
+        self.perplexity_container.layout().addWidget(self.perplexity.native)
         self.perplexity_container.setVisible(False)
 
         # select properties of which to produce a dimension reduce version
@@ -84,31 +89,55 @@ class DimensionalityReductionWidget(QWidget):
         # Run button
         run_widget = QWidget()
         run_widget.setLayout(QHBoxLayout())
-        button = QPushButton("Run")
+        run_button = QPushButton("Run")
+        run_widget.layout().addWidget(run_button)
+
+        # Update measurements button
+        update_container = QWidget()
+        update_container.setLayout(QHBoxLayout())
+        update_button = QPushButton("Update Measurements")
+        update_container.layout().addWidget(update_button)
+
+        # Defaults button
+        defaults_container = QWidget()
+        defaults_container.setLayout(QHBoxLayout())
+        defaults_button = QPushButton("Restore Defaults")
+        defaults_container.layout().addWidget(defaults_button)
 
         def run_clicked():
-            if self.get_selected_label() is None:
+
+            if self.labels_select.value is None:
                 warnings.warn("No labels image was selected!")
                 return
 
+            if len(self.properties_list.selectedItems()) == 0:
+                warnings.warn("Please select some measurements!")
+                return
+
+            if self.algorithm_choice_list.currentText() == "":
+                warnings.warn("Please select dimensionality reduction algorithm.")
+                return
+
             self.run(
-                self.get_selected_label(),
+                self.labels_select.value,
                 [i.text() for i in self.properties_list.selectedItems()],
-                self.n_neighbors.value(), self.perplexity.value(),
+                self.n_neighbors.value, self.perplexity.value,
                 self.algorithm_choice_list.currentText()
-                # todo: enter number of components here as third parameter
             )
 
-        button.clicked.connect(run_clicked)
-        run_widget.layout().addWidget(button)
+        run_button.clicked.connect(run_clicked)
+        update_button.clicked.connect(self.update_properties_list)
+        defaults_button.clicked.connect(partial(restore_defaults, self, DEFAULTS))
 
         # adding all widgets to the layout
         self.layout().addWidget(label_container)
-        self.layout().addWidget(choose_img_container)
+        self.layout().addWidget(labels_layer_selection_container)
         self.layout().addWidget(algorithm_container)
         self.layout().addWidget(self.perplexity_container)
         self.layout().addWidget(self.n_neighbors_container)
         self.layout().addWidget(choose_properties_container)
+        self.layout().addWidget(update_container)
+        self.layout().addWidget(defaults_container)
         self.layout().addWidget(run_widget)
         self.layout().setSpacing(0)
 
@@ -122,55 +151,38 @@ class DimensionalityReductionWidget(QWidget):
         self.algorithm_choice_list.currentIndexChanged.connect(self.change_neighbours_list)
         self.algorithm_choice_list.currentIndexChanged.connect(self.change_perplexity)
 
-    def get_selected_label(self):
-        index = self.label_list.currentIndex()
-        if index >= 0:
-            return self._available_labels[index]
-        return None
+    def showEvent(self, event) -> None:
+        super().showEvent(event)
+        self.reset_choices()
 
-    def update_label_list(self):
-        selected_layer = self.get_selected_label()
-        selected_index = -1
+    def reset_choices(self, event=None):
+        self.labels_select.reset_choices(event)
 
-        self._available_labels = []
-        self.label_list.clear()
-        i = 0
-        for layer in self.viewer.layers:
-            if isinstance(layer, napari.layers.Labels):
-                self._available_labels.append(layer)
-                if layer == selected_layer:
-                    selected_index = i
-                self.label_list.addItem(layer.name)
-                i = i + 1
-        self.label_list.setCurrentIndex(selected_index)
+    # toggle widgets visibility according to what is selected
+    def change_neighbours_list(self):
+        widgets_inactive(self.n_neighbors_container, active=self.algorithm_choice_list.currentText() == 'UMAP')
+
+    def change_perplexity(self):
+        widgets_inactive(self.perplexity_container, active=self.algorithm_choice_list.currentText() == 't-SNE')
 
     def update_properties_list(self):
-        selected_layer = self.get_selected_label()
-
-        print("Selected layer none?")
+        selected_layer = self.labels_select.value
         if selected_layer is not None:
-            print("Properties none?")
             properties = selected_layer.properties
             if selected_layer.properties is not None:
                 self.properties_list.clear()
                 for p in list(properties.keys()):
+                    if p == "label" or "CLUSTER_ID" in p:
+                        continue
                     item = QListWidgetItem(p)
                     self.properties_list.addItem(item)
-                    # per default select all measurements that are not "label
-                    if p != "label":
-                        item.setSelected(True)
-
-    def _on_selection(self, event=None):
-        num_labels_in_viewer = len([layer for layer in self.viewer.layers if isinstance(layer, napari.layers.Labels)])
-        if num_labels_in_viewer != self.label_list.size():
-            self.update_label_list()
+                    item.setSelected(True)
 
     # this function runs after the run button is clicked
     def run(self, labels_layer, selected_measurements_list, n_neighbours, perplexity, selected_algorithm,
             n_components=2):
-        print("Dimensionality reduction running")
-        print(labels_layer)
-        print(selected_measurements_list)
+        print("Selected labels layer: " + str(labels_layer))
+        print("Selected measurements: " + str(selected_measurements_list))
 
         # Turn properties from layer into a dataframe
         properties = labels_layer.properties
@@ -180,7 +192,8 @@ class DimensionalityReductionWidget(QWidget):
         properties_to_reduce = reg_props[selected_measurements_list]
 
         if selected_algorithm == 'UMAP':
-            # reduce dimensions
+            print("Dimensionality reduction started (" + str(selected_algorithm) + ")...")
+            # reduce dimensionality
             embedding = umap(properties_to_reduce, n_neighbours, n_components)
 
             # write result back to properties
@@ -188,31 +201,22 @@ class DimensionalityReductionWidget(QWidget):
                 properties["UMAP_" + str(i)] = embedding[:, i]
 
         elif selected_algorithm == 't-SNE':
-            # reduce dimensions
+            print("Dimensionality reduction started (" + str(selected_algorithm) + ")...")
+            # reduce dimensionality
             embedding = tsne(properties_to_reduce, perplexity, n_components)
 
             # write result back to properties
             for i in range(0, n_components):
                 properties['t-SNE_' + str(i)] = embedding[:, i]
 
-        else:
-            warnings.warn('No Dimension Reduction Algorithm Chosen!')
-
         from ._utilities import show_table
         show_table(self.viewer, labels_layer)
 
         print("Dimensionality reduction finished")
 
-    # toggle widgets visibility according to what is selected
-    def change_neighbours_list(self):
-        widgets_inactive(self.n_neighbors_container, active=self.algorithm_choice_list.currentText() == 'UMAP')
 
-    def change_perplexity(self):
-        widgets_inactive(self.perplexity_container, active=self.algorithm_choice_list.currentText() == 'TSNE')
-
-
-def umap(reg_props, n_neigh=15, n_components=2):
-    from sklearn.preprocessing import StandardScaler
+def umap(reg_props, n_neigh, n_components):  # n_components: dimension of the embedded space. For now 2 by default,
+    from sklearn.preprocessing import StandardScaler  # since only 2D plotting is supported
     import umap.umap_ as umap
 
     reducer = umap.UMAP(random_state=133, n_components=n_components, n_neighbors=n_neigh)
@@ -222,7 +226,7 @@ def umap(reg_props, n_neigh=15, n_components=2):
     return reducer.fit_transform(scaled_regionprops)
 
 
-def tsne(reg_props, perplexity, n_components=2):
+def tsne(reg_props, perplexity, n_components):
     from sklearn.preprocessing import StandardScaler
     from sklearn.manifold import TSNE
 
