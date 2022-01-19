7c3ba3cf6f9cbe53df86785db4723cda3aeab805
Laura Žigutytė <zigutyte@gmail.com>
2022-01-19 18:44:16 +0100

    Add two new functions for writing and getting layer tabular data

--- napari_clusters_plotter/_utilities.py
+++ napari_clusters_plotter/_utilities.py
@@ -12,3 +12,18 @@ def show_table(viewer, labels_layer):
 def restore_defaults(widget, defaults: dict):
     for item, val in defaults.items():
         getattr(widget, item).value = val
+
+
+def set_features(layer, tabular_data):
+    if hasattr(layer, "properties"):
+        layer.properties = tabular_data
+    if hasattr(layer, "features"):
+        layer.features = tabular_data
+
+
+def get_layer_tabular_data(layer):
+    if hasattr(layer, "features") and layer.features is not None:
+        return layer.features
+    if hasattr(layer, "properties") and layer.properties is not None:
+        return layer.properties
+    return None
