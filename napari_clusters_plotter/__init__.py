da2c6841140e66fabc806eb13e303c99ba638007
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-01-21 18:00:27 +0100

    Made widgets  public

--- napari_clusters_plotter/__init__.py
+++ napari_clusters_plotter/__init__.py
@@ -1,6 +1,8 @@
 
 __version__ = "0.2.2"
 
-
 from ._dock_widget import napari_experimental_provide_dock_widget
-
+from ._measure import MeasureWidget
+from ._plotter import PlotterWidget
+from ._dimensionality_reduction import DimensionalityReductionWidget
+from ._clustering import ClusteringWidget
