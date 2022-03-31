ecd535059a5cebcd7410534b979498005be859fb
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-03-31 15:54:55 +0200

    added feature check in test

--- napari_clusters_plotter/_tests/test_utils.py
+++ napari_clusters_plotter/_tests/test_utils.py
@@ -31,7 +31,10 @@ def test_feature_setting(make_napari_viewer):
     some_features = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
     set_features(label_layer, some_features)
 
-    assert isinstance(label_layer.features, pd.DataFrame)
+    if hasattr(label_layer, "features"):
+        assert isinstance(label_layer.features, pd.DataFrame)
+    elif hasattr(label_layer, "properties"):
+        assert isinstance(label_layer.properties, dict)
 
     some_features = get_layer_tabular_data(label_layer)
     assert isinstance(some_features, pd.DataFrame)
@@ -42,4 +45,6 @@ def test_feature_setting(make_napari_viewer):
 
 
 if __name__ == "__main__":
-    test_feature_setting()
+    import napari
+
+    test_feature_setting(napari.Viewer)
