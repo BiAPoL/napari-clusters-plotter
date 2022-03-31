2938d3b6efed86c8865d9fb909ffcd6e3aa4cc89
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-03-31 15:57:24 +0200

    Added return type annotation

--- napari_clusters_plotter/_dimensionality_reduction.py
+++ napari_clusters_plotter/_dimensionality_reduction.py
@@ -1,6 +1,7 @@
 import warnings
 from functools import partial
 
+import numpy as np
 import pandas as pd
 from magicgui.widgets import create_widget
 from napari.layers import Labels
@@ -459,7 +460,9 @@ class DimensionalityReductionWidget(QWidget):
 
 
 @catch_NaNs
-def umap(reg_props: pd.DataFrame, n_neigh: int, n_components: int, standardize: bool):
+def umap(
+    reg_props: pd.DataFrame, n_neigh: int, n_components: int, standardize: bool
+) -> np.ndarray:
     import umap.umap_ as umap
 
     reducer = umap.UMAP(
@@ -478,7 +481,7 @@ def umap(reg_props: pd.DataFrame, n_neigh: int, n_components: int, standardize: 
 @catch_NaNs
 def tsne(
     reg_props: pd.DataFrame, perplexity: float, n_components: int, standardize: bool
-):
+) -> np.ndarray:
     from sklearn.manifold import TSNE
 
     reducer = TSNE(
@@ -501,7 +504,7 @@ def tsne(
 @catch_NaNs
 def pca(
     reg_props: pd.DataFrame, explained_variance_threshold: float, n_components: int
-):
+) -> np.ndarray:
     from sklearn.decomposition import PCA
     from sklearn.preprocessing import StandardScaler
 
