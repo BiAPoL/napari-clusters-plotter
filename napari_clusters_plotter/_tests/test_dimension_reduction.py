3d97b75e91230d40172680d973263a4ce6d700c8
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-03-31 15:42:25 +0200

    Added test for bad measurement

--- napari_clusters_plotter/_tests/test_dimension_reduction.py
+++ napari_clusters_plotter/_tests/test_dimension_reduction.py
@@ -26,6 +26,47 @@ def test_clustering_widget(make_napari_viewer):
     assert len(viewer.window._dock_widgets) == n_wdgts + 1
 
 
+def test_bad_measurements(make_napari_viewer):
+
+    from napari_clusters_plotter._dimensionality_reduction import (
+        DimensionalityReductionWidget,
+    )
+    from napari_clusters_plotter._measure import get_regprops_from_regprops_source
+    from napari_clusters_plotter._utilities import set_features
+
+    label = np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 1, 0, 0, 2, 2],
+            [0, 0, 0, 0, 2, 2, 2],
+            [3, 3, 0, 0, 0, 0, 0],
+            [0, 0, 4, 4, 0, 0, 0],
+            [6, 6, 6, 6, 0, 5, 0],  # <-single pixel label leading to NaN meas.
+            [0, 7, 7, 0, 0, 0, 0],
+        ]
+    )
+
+    viewer = make_napari_viewer()
+    labels_layer = viewer.add_labels(label)
+
+    image = np.random.random(label.shape)
+    measurements = get_regprops_from_regprops_source(image, label, "shape + intensity")
+    set_features(labels_layer, measurements)
+
+    widget = DimensionalityReductionWidget(napari_viewer=viewer)
+    widget.run(
+        labels_layer=labels_layer,
+        selected_measurements_list=list(measurements.keys()),
+        n_neighbours=2,
+        perplexity=5,
+        selected_algorithm="UMAP",
+        standardize=False,
+        n_components=2,
+        explained_variance=95.0,
+        pca_components=0,
+    )
+
+
 def test_call_to_function(make_napari_viewer):
 
     viewer = make_napari_viewer()
@@ -146,5 +187,7 @@ def test_pca():
 
 
 if __name__ == "__main__":
-    test_clustering_widget()
-    test_call_to_function()
+    import napari
+
+    # test_clustering_widget()
+    test_bad_measurements(napari.Viewer)
