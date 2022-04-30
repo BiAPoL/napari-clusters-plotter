31e759e88467d5611607160e0271b3c7efa0da33
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-04-30 16:42:24 +0200

    changed input types, added support to add result to viewer

--- napari_clusters_plotter/_annotation_to_clusters.py
+++ napari_clusters_plotter/_annotation_to_clusters.py
@@ -1,7 +1,6 @@
-from ._utilities import add_column_to_layer_tabular_data, show_table
+from ._utilities import add_column_to_layer_tabular_data
 from skimage.measure import regionprops_table
-from napari_clusters_plotter._utilities import show_table
-from napari.layers import Labels
+from napari.types import LabelsData
 from napari import Viewer
 from napari_tools_menu import register_function
 
@@ -9,18 +8,24 @@ from napari_tools_menu import register_function
 @register_function(
     menu="Measurement > Convert annotation to cluster ID (ncp)"
 )
-def annotation_to_cluster_id(Label_Layer: Labels = None, Annotation_Layer: Labels = None, viewer: Viewer = None, Unannotated_Objects_Clustered: bool = False) -> None:
-    label_image = Label_Layer.data
-    annotation = Annotation_Layer.data
+def annotation_to_cluster_id(label_image: LabelsData, annotation: LabelsData, viewer: Viewer = None) -> None:
     regionproperties = regionprops_table(label_image, intensity_image=annotation, properties=('label', 'intensity_max'))
-    if Unannotated_Objects_Clustered:
-        data = regionproperties['intensity_max']
+    data = regionproperties['intensity_max']
+
+    if viewer is not None:
+        # store the layer for saving results later
+        from napari_workflows._workflow import _get_layer_from_data
+        labels_layer = _get_layer_from_data(viewer, label_image)
+
+        # Store results in the properties dictionary:
+        add_column_to_layer_tabular_data(
+            layer=labels_layer,
+            column_name="Annotation_CLUSTER_ID",
+            data=data
+        )
+
+        # turn table into a widget
+        from napari_skimage_regionprops import add_table
+        add_table(labels_layer, viewer)
     else:
-        data = regionproperties['intensity_max']-1
-    
-    add_column_to_layer_tabular_data(
-        layer=Label_Layer,
-        column_name="Annotation_CLUSTER_ID",
-        data=data
-    )
-    show_table(labels_layer=Label_Layer, viewer=viewer)
+        return data
