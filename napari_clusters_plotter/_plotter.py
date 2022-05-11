bcdbbc549ffc0c4515c47d0eaf9a5aed2bc20d53
Cryaaa <ryan.savill@web.de>
2022-05-11 15:28:51 +0200

    finishing the experiment

--- napari_clusters_plotter/_plotter.py
+++ napari_clusters_plotter/_plotter.py
@@ -555,7 +555,7 @@ class PlotterWidget(QWidget):
                     if prediction >= 0
                     else [0, 0, 0, 0]
                 )
-                for prediction in self.cluster_ids
+                for prediction in set(self.cluster_ids)
             }
             # take care of background label
             cmap_dict[0] = [0, 0, 0, 0]
