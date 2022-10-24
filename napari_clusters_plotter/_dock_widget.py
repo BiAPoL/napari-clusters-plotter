e8cc99ccd4ac9a8b11227c3bdd5040e9ad1cca89
Cryaaa <ryan.savill@web.de>
2022-10-24 19:13:44 +0200

    matplotlib import needed, removed measurement completely

--- napari_clusters_plotter/_dock_widget.py
+++ napari_clusters_plotter/_dock_widget.py
@@ -2,14 +2,12 @@ from napari_plugin_engine import napari_hook_implementation
 
 from ._clustering import ClusteringWidget
 from ._dimensionality_reduction import DimensionalityReductionWidget
-from ._measure import MeasureWidget
 from ._plotter import PlotterWidget
 
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
     return [
-        MeasureWidget,
         PlotterWidget,
         DimensionalityReductionWidget,
         ClusteringWidget,
