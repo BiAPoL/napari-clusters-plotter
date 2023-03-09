89f63a4eeccff56421cfe652a3f8c6023ed03da1
thorstenwagner <twa1@posteo.de>
2023-03-09 13:32:11 +0100

    add some type hints

--- napari_clusters_plotter/_Qt_code.py
+++ napari_clusters_plotter/_Qt_code.py
@@ -2,7 +2,9 @@ import os
 from pathlib import Path as PathL
 
 import numpy as np
+import numpy.typing
 import pandas as pd
+import typing
 from magicgui.widgets import create_widget
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
@@ -512,7 +514,7 @@ class MplCanvas(FigureCanvas):
         self.axes.clear()
         self.is_pressed = None
 
-    def make_2d_histogram(self, data_x, data_y, colors, bin_number=400):
+    def make_2d_histogram(self, data_x: "numpy.typing.ArrayLike", data_y: "numpy.typing.ArrayLike", colors: "typing.List[str]", bin_number: int=400):
         heatmap, xedges, yedges = np.histogram2d(data_x, data_y, bins=bin_number)
         extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
         if len(colors) == 1:
@@ -530,7 +532,7 @@ class MplCanvas(FigureCanvas):
         self.selector.disconnect()
         self.selector = SelectFrom2DHistogram(self, self.axes, full_data)
 
-    def make_scatter_plot(self, data_x, data_y, colors, sizes, alpha):
+    def make_scatter_plot(self, data_x: "numpy.typing.ArrayLike", data_y: "numpy.typing.ArrayLike", colors: "typing.List[str]", sizes: "typing.List[float]", alpha: "typing.List[float]"):
         self.pts = self.axes.scatter(
             data_x,
             data_y,
