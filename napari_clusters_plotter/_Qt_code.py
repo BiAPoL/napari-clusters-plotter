eca02857e0a51b84b803ab4a05a7085b7cbde34a
thorstenwagner <twa1@posteo.de>
2023-03-15 09:09:13 +0100

    set plot size to 7 and 4 again

--- napari_clusters_plotter/_Qt_code.py
+++ napari_clusters_plotter/_Qt_code.py
@@ -460,7 +460,7 @@ class SelectFromCollection:
 
 
 class MplCanvas(FigureCanvas):
-    def __init__(self, parent=None, width=10, height=8, manual_clustering_method=None):
+    def __init__(self, parent=None, width=7, height=4, manual_clustering_method=None):
         self.fig = Figure(figsize=(width, height), constrained_layout=True)
         self.manual_clustering_method = manual_clustering_method
 
