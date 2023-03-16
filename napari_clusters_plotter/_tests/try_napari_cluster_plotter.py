2e199a8fb5fd61b2cd76b8905034450a51b1b7ea
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2023-03-16 11:51:28 +0100

    Add napari run to try script

--- napari_clusters_plotter/_tests/try_napari_cluster_plotter.py
+++ napari_clusters_plotter/_tests/try_napari_cluster_plotter.py
@@ -1,14 +1,18 @@
 import napari
 import napari_process_points_and_surfaces as nppas
+from napari_clusters_plotter._plotter import PlotterWidget
+
 
 viewer = napari.Viewer(ndisplay=3)
 
 gastruloid = nppas.gastruloid()
 
 requested_measurements = [nppas.Quality.ASPECT_RATIO, nppas.Quality.AREA]
-df = nppas.surface_quality_table(gastruloid, requested_measurements)
+data_frame = nppas.surface_quality_table(gastruloid, requested_measurements)
 surface_layer = viewer.add_surface(gastruloid)
-surface_layer.properties = df.to_dict(orient='list')
-surface_layer.features = df
+surface_layer.properties = data_frame.to_dict(orient='list')
+surface_layer.features = data_frame
 
+viewer.window.add_dock_widget(PlotterWidget(viewer), area='right')
 
+napari.run()
