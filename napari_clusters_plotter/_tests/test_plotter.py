9cf67049d4222163e2f9c08f4c46778eddb52729
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2024-04-23 11:55:24 +0200

    added test for 4d surface

--- napari_clusters_plotter/_tests/test_plotter.py
+++ napari_clusters_plotter/_tests/test_plotter.py
@@ -311,6 +311,53 @@ def test_plotter_on_surface_data(make_napari_viewer):
     )
 
 
+def test_plotter_on_surface_data4d(make_napari_viewer):
+
+    from napari_clusters_plotter._plotter import PlotterWidget
+
+    viewer = make_napari_viewer()
+    n_points = 100
+    n_timepoints = 2
+    vertices = np.random.rand(n_points, 4)
+    vertices[:, 0] = np.arange(0, n_timepoints).repeat(n_points // n_timepoints)
+
+    # Create a random mesh
+    faces_t0 = np.random.randint(0, 49, (100, 3))
+    faces_t1 = np.random.randint(50, 99, (100, 3))
+    faces = np.concatenate((faces_t0, faces_t1))
+
+    feature1 = np.random.normal(size=n_points)
+    feature2 = np.random.normal(size=n_points, loc=1)
+    annotations = 1 * (np.random.uniform(size=n_points) > 0.5)
+    surface_tuple = (vertices, faces)
+
+    # need to add features later-on as napari currently doesn't support features for surfaces
+    layer = viewer.add_surface(surface_tuple, name='random_mesh')
+    layer.features = pd.DataFrame({'feature1': feature1, 'feature2': feature2, 'frame': vertices[:, 0], 'annotations': annotations})
+
+    widget = PlotterWidget(viewer)
+    viewer.window.add_dock_widget(widget)
+
+    # Put the points layer into the layer selection widget
+    widget.analysed_layer = viewer.layers[0]
+    widget.plot_x_axis.setCurrentText("feature1")
+    widget.plot_y_axis.setCurrentText("feature2")
+    widget.analysed_layer.features["MANUAL_CLUSTER_ID"] = annotations
+
+    # check that the features are found
+    features = get_layer_tabular_data(widget.analysed_layer)
+    assert "feature1" in features.columns
+    assert "feature2" in features.columns
+
+    widget.run(
+        widget.analysed_layer.features,
+        plot_x_axis_name="feature1",
+        plot_y_axis_name="feature2",
+        plot_cluster_name="MANUAL_CLUSTER_ID",
+        force_redraw=True,
+    )
+
+
 def test_cluster_image_generation_for_histogram(make_napari_viewer):
     from napari_clusters_plotter._plotter import PlotterWidget
 
