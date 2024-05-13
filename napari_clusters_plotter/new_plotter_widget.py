226f05a5d8d9015c9774c4ffff708d14bfa783f6
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2024-05-13 14:12:24 +0200

    added semi-public callback functions
    used correct attribute function calls

--- napari_clusters_plotter/new_plotter_widget.py
+++ napari_clusters_plotter/new_plotter_widget.py
@@ -53,46 +53,7 @@ class PlotterWidget(QMainWindow):
         self.layout = QVBoxLayout(self.contents)
         self.layout.setAlignment(Qt.AlignTop)
 
-        self.plotting_widget = PlotWidget(napari_viewer, self)
-
-        def replot():
-            # redraw the whole plot
-            try:
-                # plotting function needs to be here
-                pass
-
-            except AttributeError:
-                # In this case, replotting is not yet possible
-                pass
-
-        def checkbox_status_changed():
-            replot()
-
-        def plotting_type_changed():  # TODO NEED TO ADD WHICH VARIABLE STORES THE TYPE
-            if (
-                self.control_widget.plot_type_box.currentText()
-                == PlottingType.HISTOGRAM.name
-            ):
-                self.control_widget.bins_settings_container.setVisible(True)
-                self.control_widget.log_scale_container.setVisible(True)
-            elif (
-                self.control_widget.plot_type_box.currentText()
-                == PlottingType.SCATTER.name
-            ):
-                self.control_widget.bins_settings_container.setVisible(False)
-                self.control_widget.log_scale_container.setVisible(False)
-
-            replot()
-
-        def bin_number_set():
-            replot()
-
-        def bin_auto():
-            self.control_widget.manual_bins_container.setVisible(
-                not self.control_widget.auto_bins_checkbox.isChecked()
-            )
-            if self.control_widget.auto_bins_checkbox.isChecked():
-                replot()
+        self.plotting_widget = CanvasWidget(napari_viewer, self)
 
         # Add plot and options as widgets
         self.layout.addWidget(self.plotting_widget)
@@ -121,20 +82,22 @@ class PlotterWidget(QMainWindow):
 
         # Adding Connections
         self.control_widget.plot_type_box.currentIndexChanged.connect(
-            plotting_type_changed
+            self._on_plotting_type_changed
         )
-        self.control_widget.set_bins_button.clicked.connect(bin_number_set)
-        self.control_widget.auto_bins_checkbox.stateChanged.connect(bin_auto)
-        self.control_widget.log_scale_checkbutton.stateChanged.connect(replot)
+
+        self.control_widget.plotbuttons.clicked.connect(self.replot)
+        self.control_widget.set_bins_button.clicked.connect(self._on_bin_number_set)
+        self.control_widget.auto_bins_checkbox.stateChanged.connect(self._on_bin_auto)
+        self.control_widget.log_scale_checkbutton.stateChanged.connect(self.replot)
         self.control_widget.non_selected_checkbutton.stateChanged.connect(
-            checkbox_status_changed
+            self._on_checkbox_status_changed
         )
-        self.control_widget.cmap_box.currentIndexChanged.connect(replot)
+        self.control_widget.cmap_box.currentIndexChanged.connect(self.replot)
         self.control_widget.x_axis_box.currentIndexChanged.connect(
-            replot
+            self.replot
         )  # TODO Decide if this is a good idea
         self.control_widget.y_axis_box.currentIndexChanged.connect(
-            replot
+            self.replot
         )  # TODO Decide if this is a good idea
 
         # initialising all variables
@@ -148,6 +111,73 @@ class PlotterWidget(QMainWindow):
         self.plotting_type = self.control_widget.plot_type_box.currentText()
         # self.x_axis = self.control_widget.x_axis_box.currentText()
         # self.y_axis = self.control_widget.y_axis_box.currentText()
+
+    def replot(self):
+        # redraw the whole plot
+        try:
+            # Get the currently selected items in the QComboBoxes
+            x_axis = self.control_widget.x_axis_box.currentText()
+            y_axis = self.control_widget.y_axis_box.currentText()
+
+            # Get the currently selected layer's features
+            features = self.viewer.layers.selection.active.features
+
+            # If the features are not None
+            if features is not None:
+                # Index into the dataframe to get the two series
+                x_data = features[x_axis].values
+                y_data = features[y_axis].values
+
+                # Set the plotting_widget's data attribute to a tuple of these two series
+                self.plotting_widget.data = np.stack((x_data, y_data), axis=1)
+
+        except AttributeError:
+            # In this case, replotting is not yet possible
+            print(traceback.format_exc())
+            pass
+
+    def _on_update_axis_boxes(self):
+        # Clear the current items in the QComboBoxes
+        self.control_widget.x_axis_box.clear()
+        self.control_widget.y_axis_box.clear()
+
+        # Get the currently selected layer's features
+        features = self.viewer.layers.selection.active.features
+
+        # If the features are not None
+        if features is not None:
+            # Iterate over the column names of the dataframe
+            for column_name in features.columns:
+                # Add the column name as an item to the QComboBoxes
+                self.control_widget.x_axis_box.addItem(column_name)
+                self.control_widget.y_axis_box.addItem(column_name)
+
+    def _on_checkbox_status_changed(self):
+        self.replot()
+
+    def _on_plotting_type_changed(self):  # TODO NEED TO ADD WHICH VARIABLE STORES THE TYPE
+        plot_type = self.control_widget.plot_type_box.currentText()
+        if (plot_type == PlottingType.HISTOGRAM.name):
+            self.control_widget.bins_settings_container.setVisible(True)
+            self.control_widget.log_scale_container.setVisible(True)
+            self.plotting_widget.active_artist = PlottingType.HISTOGRAM
+
+        elif (plot_type == PlottingType.SCATTER.name):
+            self.control_widget.bins_settings_container.setVisible(False)
+            self.control_widget.log_scale_container.setVisible(False)
+            self.plotting_widget.active_artist = PlottingType.SCATTER
+
+        self.replot()
+
+    def _on_bin_number_set(self):
+        self.replot()
+
+    def _on_bin_auto(self):
+        self.control_widget.manual_bins_container.setVisible(
+            not self.control_widget.auto_bins_checkbox.isChecked()
+        )
+        if self.control_widget.auto_bins_checkbox.isChecked():
+            self.replot()
 
     # Connecting the widgets to actual object variables:
     # using getters and setters for flexibility
@@ -200,7 +230,9 @@ class PlotterWidget(QMainWindow):
     def x_axis(self, column: str):
         self.control_widget.x_axis_box.setCurrentText(
             column
-        )  # TODO insert checks and change values
+        )
+        # TODO insert checks and change values
+        self.replot()
 
     @property
     def y_axis(self):
