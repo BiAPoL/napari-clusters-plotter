388366dbd8e1814a3816b896a42596970db9bdf7
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2024-07-22 14:29:06 +0200

    added clustering algorithms

--- napari_clusters_plotter/_algorithms.py
+++ napari_clusters_plotter/_algorithms.py
@@ -70,3 +70,88 @@ def reduce_umap(
         reducer = umap.UMAP(n_components=n_components, n_neighbors=n_neighbors)
         return reducer.fit_transform(data)
     return _reduce_umap(data, n_components, n_neighbors, scale)
+
+def cluster_kmeans(
+        data: pd.DataFrame,
+        n_clusters: int = 3,
+        scale: bool = True) -> FunctionWorker[pd.Series]:
+    """
+    Cluster the data using KMeans
+    """
+
+    @thread_worker(progress=True)
+    def _cluster_kmeans(data: pd.DataFrame, n_clusters: int, scale: bool) -> pd.Series:
+        from sklearn.cluster import KMeans
+        from sklearn.preprocessing import StandardScaler
+
+        if scale:
+            data = StandardScaler().fit_transform(data)
+
+        kmeans = KMeans(n_clusters=n_clusters)
+        return kmeans.fit_predict(data)
+
+    return _cluster_kmeans(data, n_clusters, scale)
+
+def cluster_hdbscan(
+        data: pd.DataFrame,
+        min_cluster_size: int = 5,
+        min_samples: int = 5,
+        scale: bool = True) -> FunctionWorker[pd.Series]:
+    """
+    Cluster the data using HDBSCAN
+    """
+
+    @thread_worker(progress=True)
+    def _cluster_hdbscan(data: pd.DataFrame, min_cluster_size: int, min_samples: int, scale: bool) -> pd.Series:
+        from sklearn.cluster import HDBSCAN
+        from sklearn.preprocessing import StandardScaler
+
+        if scale:
+            data = StandardScaler().fit_transform(data)
+
+        clusterer = HDBSCAN(min_cluster_size=min_cluster_size, min_samples=min_samples)
+        return clusterer.fit_predict(data)
+
+    return _cluster_hdbscan(data, min_cluster_size, min_samples, scale)
+
+def  cluster_gaussian_mixture(
+        data: pd.DataFrame,
+        n_components: int = 3,
+        scale: bool = True) -> FunctionWorker[pd.Series]:
+    """
+    Cluster the data using Gaussian Mixture
+    """
+
+    @thread_worker(progress=True)
+    def _cluster_gaussian_mixture(data: pd.DataFrame, n_components: int, scale: bool) -> pd.Series:
+        from sklearn.mixture import GaussianMixture
+        from sklearn.preprocessing import StandardScaler
+
+        if scale:
+            data = StandardScaler().fit_transform(data)
+
+        gmm = GaussianMixture(n_components=n_components)
+        return gmm.fit_predict(data)
+
+    return _cluster_gaussian_mixture(data, n_components, scale)
+
+def cluster_spectral(
+        data: pd.DataFrame,
+        n_clusters: int = 3,
+        scale: bool = True) -> FunctionWorker[pd.Series]:
+    """
+    Cluster the data using Spectral Clustering
+    """
+
+    @thread_worker(progress=True)
+    def _cluster_spectral(data: pd.DataFrame, n_clusters: int, scale: bool) -> pd.Series:
+        from sklearn.cluster import SpectralClustering
+        from sklearn.preprocessing import StandardScaler
+
+        if scale:
+            data = StandardScaler().fit_transform(data)
+
+        clusterer = SpectralClustering(n_clusters=n_clusters)
+        return clusterer.fit_predict(data)
+
+    return _cluster_spectral(data, n_clusters, scale)
