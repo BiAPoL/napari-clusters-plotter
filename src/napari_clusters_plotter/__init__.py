fa196ba315880c0c844c5309e0a3abb3c8f4ecee
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2024-08-08 17:10:22 +0200

    added new widgets to `__all__`

--- src/napari_clusters_plotter/__init__.py
+++ src/napari_clusters_plotter/__init__.py
@@ -2,3 +2,5 @@ __version__ = "0.8.0"
 
 from ._new_plotter_widget import PlotterWidget
 from ._dim_reduction_and_clustering import DimensionalityReductionWidget, ClusteringWidget
+
+__all__ = ['PlotterWidget', 'DimensionalityReductionWidget', 'ClusteringWidget']
