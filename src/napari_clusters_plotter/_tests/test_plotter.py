1c58e121c96d46f69e369f9b8ab00943e1cafa80
Johannes Müller <johannes.mueller88@hotmail.de>
2025-01-29 21:11:40 +0100

    added test for cluster memorization

--- src/napari_clusters_plotter/_tests/test_plotter.py
+++ src/napari_clusters_plotter/_tests/test_plotter.py
@@ -1,4 +1,39 @@
 import numpy as np
+import pytest
+
+
+def create_multi_point_layer(n_samples: int = 100):
+    import pandas as pd
+    from napari.layers import Points
+
+    loc = 5
+    n_timeframes = 5
+    frame = np.arange(n_timeframes).repeat(n_samples//n_timeframes)
+    # make some random points with random  features
+    points = np.random.random((n_samples, 4))
+    points2 = np.random.random((n_samples, 4))
+
+    points[:, 0] = frame
+    points2[:, 0] = frame
+
+    features = pd.DataFrame({
+        'frame': frame,
+        'feature1': np.random.normal(size=n_samples, loc=loc),
+        'feature2': np.random.normal(size=n_samples, loc=loc),
+        'feature3': np.random.normal(size=n_samples, loc=loc),
+        'feature4': np.random.normal(size=n_samples, loc=loc),})
+
+    features2 = pd.DataFrame({
+        'frame': frame,
+        'feature2': np.random.normal(size=n_samples, loc=-loc),
+        'feature3': np.random.normal(size=n_samples, loc=-loc),
+        'feature4': np.random.normal(size=n_samples, loc=-loc),})
+
+    layer = Points(points, features=features, size=0.1, blending='translucent_no_depth')
+    layer2 = Points(points2, features=features2, size=0.1, translate=(0, 0, 2), blending='translucent_no_depth')
+
+    return layer, layer2
+
 
 
 def test_mixed_layers(make_napari_viewer):
@@ -25,3 +60,30 @@ def test_mixed_layers(make_napari_viewer):
     viewer.add_labels(sample_labels)
 
     #
+
+
+def test_cluster_memorization(make_napari_viewer, n_samples: int = 100):
+    from napari_clusters_plotter import PlotterWidget
+
+    viewer = make_napari_viewer()
+    layer, layer2 = create_multi_point_layer(n_samples=n_samples)
+
+    # add layers to viewer
+    viewer.add_layer(layer)
+    viewer.add_layer(layer2)
+    plotter_widget = PlotterWidget(viewer)
+    viewer.window.add_dock_widget(plotter_widget, area='right')
+
+    # select last layer and create a random cluster selection
+    viewer.layers.selection.active = viewer.layers[-1]
+    plotter_widget._selectors['x'].setCurrentText('feature3')
+    cluster_indeces = np.random.randint(0, 2, n_samples)
+    plotter_widget.plotting_widget.active_artist.color_indices = cluster_indeces
+    
+    # select first layer and make sure that no clusters are selected
+    viewer.layers.selection.active = viewer.layers[0]
+    assert np.all(plotter_widget.plotting_widget.active_artist.color_indices == 0)
+
+    # select last layer and make sure that the clusters are the same
+    viewer.layers.selection.active = viewer.layers[-1]
+    assert np.all(plotter_widget.plotting_widget.active_artist.color_indices == cluster_indeces)
