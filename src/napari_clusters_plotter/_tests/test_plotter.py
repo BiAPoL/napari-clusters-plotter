0ac3bfb7c4f05725c0791fca358edd29f2eb879e
Johannes Soltwedel <38459088+jo-mueller@users.noreply.github.com>
2025-04-15 08:49:22 +0200

    created more sample data functions for test

--- src/napari_clusters_plotter/_tests/test_plotter.py
+++ src/napari_clusters_plotter/_tests/test_plotter.py
@@ -48,6 +48,57 @@ def create_multi_point_layer(n_samples: int = 100):
     return layer, layer2
 
 
+def create_multi_vectors_layer(n_samples: int = 100):
+    from napari.layers import Vectors
+
+    points1, points2 = create_multi_point_layer(n_samples=n_samples)
+
+    points_direction1 = np.random.normal(size=points1.data.shape)
+    points_direction2 = np.random.normal(size=points2.data.shape)
+
+    # set time index correctly
+    points_direction1[:, 0] = points1.data[:, 0]
+    points_direction2[:, 0] = points2.data[:, 1]
+
+    vectors1 = np.stack([points1.data, points_direction1], axis=1)
+    vectors2 = np.stack([points2.data, points_direction2], axis=1)
+
+    vectors1 = Vectors(vectors1, features=points1.features, name="vectors1")
+    vectors2 = Vectors(vectors2, features=points2.features, name="vectors2")
+
+    return vectors1, vectors2
+
+
+def create_multi_labels_layer():
+    from skimage import data, measure
+    from napari.layers import Labels
+    import pandas as pd
+
+    labels1 = measure.label(data.binary_blobs(length=64, n_dim=2))
+    labels2 = measure.label(data.binary_blobs(length=64, n_dim=2))
+
+    features1 = pd.DataFrame(
+        {
+            "feature1": np.random.normal(size=labels1.max() + 1),
+            "feature2": np.random.normal(size=labels1.max() + 1),
+            "feature3": np.random.normal(size=labels1.max() + 1),
+        }
+    )
+
+    features2 = pd.DataFrame(
+        {
+            "feature1": np.random.normal(size=labels2.max() + 1),
+            "feature2": np.random.normal(size=labels2.max() + 1),
+            "feature3": np.random.normal(size=labels2.max() + 1),
+        }
+    )
+
+    labels1 = Labels(labels1, name="labels1", features=features1)
+    labels2 = Labels(labels2, name="labels2", features=features2, translate=(0, 128))
+
+    return labels1, labels2
+
+
 def test_mixed_layers(make_napari_viewer):
     from napari_clusters_plotter import PlotterWidget
 
