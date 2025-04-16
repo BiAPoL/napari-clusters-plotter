db940b7f6564ba05fa12d5e8252fea40449b5c18
Johannes Soltwedel <38459088+jo-mueller@users.noreply.github.com>
2025-04-16 22:13:17 +0200

    added test for sample data

--- src/napari_clusters_plotter/_tests/test_sample_data.py
+++ src/napari_clusters_plotter/_tests/test_sample_data.py
@@ -1,4 +1,4 @@
-def test_sample_data(make_napari_viewer):
+def test_bbbc_1_sample_data(make_napari_viewer):
     from napari.layers import Layer
 
     import napari_clusters_plotter as ncp
@@ -6,6 +6,21 @@ def test_sample_data(make_napari_viewer):
     viewer = make_napari_viewer()
 
     sample_dataset = ncp.bbbc_1_dataset()
+    for sample in sample_dataset:
+        layer = Layer.create(sample[0], sample[1], sample[2])
+        viewer.add_layer(layer)
+
+    assert len(viewer.layers) == len(sample_dataset)
+
+
+def test_tgmm_mini_dataset(make_napari_viewer):
+    from napari.layers import Layer
+
+    import napari_clusters_plotter as ncp
+
+    viewer = make_napari_viewer()
+
+    sample_dataset = ncp.tgmm_mini_dataset()
     for sample in sample_dataset:
         layer = Layer.create(sample[0], sample[1], sample[2])
         viewer.add_layer(layer)
