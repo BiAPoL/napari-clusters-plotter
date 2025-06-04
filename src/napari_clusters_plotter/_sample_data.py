4f6233726688b92416f17f8bddca6f53ca6d9c46
Johannes Soltwedel <38459088+jo-mueller@users.noreply.github.com>
2025-06-04 22:24:40 +0200

    replace translate grid by napari grid view for bbbc1 dataset

--- src/napari_clusters_plotter/_sample_data.py
+++ src/napari_clusters_plotter/_sample_data.py
@@ -42,9 +42,9 @@ def tgmm_mini_dataset() -> List["LayerData"]:  # noqa: F821
 
 
 def bbbc_1_dataset() -> List["LayerData"]:  # noqa: F821
-    import numpy as np
     import pandas as pd
     from skimage import io
+    import napari
 
     # get path of this file
     path = Path(__file__).parent / "sample_data" / "BBBC007_v1_images"
@@ -53,12 +53,7 @@ def bbbc_1_dataset() -> List["LayerData"]:  # noqa: F821
         os.path.join(str(path), "**", "*.tif"), recursive=True
     )
     raw_images = [f for f in tif_files if "labels" not in f]
-
-    spacing = 500
-    # calculate positions of images on grid
-    n_cols = np.sqrt(len(raw_images))
-    image_layers = []
-    labels_layers = []
+    layers = []
 
     for i, raw_image_filename in enumerate(raw_images):
 
@@ -67,16 +62,12 @@ def bbbc_1_dataset() -> List["LayerData"]:  # noqa: F821
         image = io.imread(raw_image_filename)
         labels = io.imread(label_filename)
 
-        row = i // n_cols
-        col = i % n_cols
-
         features = pd.read_csv(feature_filename)
 
         ldtuple_image = (
             image,
             {
                 "name": Path(raw_image_filename).stem,
-                "translate": [spacing * col, spacing * row],
             },
             "image",
         )
@@ -85,16 +76,19 @@ def bbbc_1_dataset() -> List["LayerData"]:  # noqa: F821
             labels,
             {
                 "name": Path(raw_image_filename).stem + "_labels",
-                "translate": [spacing * col, spacing * row],
                 "features": features,
             },
             "labels",
         )
 
-        image_layers.append(ldtuple_image)
-        labels_layers.append(ldtuple_labels)
+        layers.append(ldtuple_image)
+        layers.append(ldtuple_labels)
 
-    return image_layers + labels_layers
+    viewer = napari.current_viewer()
+    viewer.grid.enabled = True
+    viewer.grid.stride = 2
+
+    return layers
 
 
 def cells3d_curvatures() -> List["LayerData"]:  # noqa: F821
